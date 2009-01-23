484a84a8b351a199c8df51825f76b2ff74f6a6c3
Johannes Krude <johannes@krude.de>
2009-01-23 01:21:37 +0100

    added some exception handling to ruby-web

--- client/ruby-web/multitrisclients.rb
+++ client/ruby-web/multitrisclients.rb
@@ -15,6 +15,7 @@ class MultitrisClients
 			@connections[cookie]= TCPSocket.new(@server, @port)
 		rescue Errno::ECONNREFUSED
 			userClose(cookie, "no server found");
+			return
 		end
 		Thread.new do
 			begin
@@ -37,12 +38,16 @@ class MultitrisClients
 	end
 
 	def userTransmit(cookie, string)
-		@connections[cookie].puts(string)
+		begin
+			@connections[cookie].puts(string)
+		rescue Errno::EPIPE
+			userCLose(cookie, "connection unexpected lost");
+		end
 	end
 
 	def userReceive(cookie)
 		result= @queues[cookie].shift || ""
-		if @connections[cookie].closed? and @queues[cookie].size == 0
+		if @connections[cookie] and @connections[cookie].closed? and @queues[cookie].size == 0
 			@connections.delete(cookie)
 			@queues.delete(cookie)
 		end
@@ -50,9 +55,9 @@ class MultitrisClients
 	end
 
 	def userClose(cookie, reason= "")
-		@queues[cookie] << "FUCKYOU " + reason
+		@queues[cookie]= ["FUCKYOU " + reason]
 		begin # FUCKYOU before close, so other threads won't delete this user to early
-			@connections[cookie].close
+			@connections[cookie].close if @connections[cookie]
 		rescue IOError
 		end
 	end
